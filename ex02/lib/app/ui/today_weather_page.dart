1891c88975cfee8770c4f575831bef7b85ef919c
Vitor Nascimento <bielvitor2008@hotmail.com>
2023-08-10 10:04:31 -0300

    feat: Implement today weather

--- ex02/lib/app/ui/today_weather_page.dart
+++ ex02/lib/app/ui/today_weather_page.dart
@@ -1,22 +1,59 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:ex00/app/domain/controllers/today_weather_controller.dart';
 import 'package:ex00/app/domain/models/place.dart';
+import 'package:ex00/app/domain/models/today_weather.dart';
 import 'package:flutter/material.dart';
 
-class TodayWeatherPage extends StatelessWidget {
+class TodayWeatherPage extends StatefulWidget {
   final Place _place;
+  bool _isLoading = true;
 
-  const TodayWeatherPage({
+  TodayWeatherPage({
     super.key,
     required Place place,
   }) : _place = place;
 
   @override
+  State<TodayWeatherPage> createState() => _TodayWeatherPageState();
+}
+
+class _TodayWeatherPageState extends State<TodayWeatherPage> {
+  late TodayWeatherController _controller;
+  late TodayWeather _todayWeather;
+
+  @override
+  void initState() {
+    super.initState();
+    widget._isLoading = true;
+    _controller = TodayWeatherController(
+        onTodayWeatherSuccess: (todayWeather) {
+          setState(() {
+            _todayWeather = todayWeather;
+            widget._isLoading = false;
+          });
+        },
+        onTodayWeatherError: () {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (widget._isLoading) {
+      _controller.getTodayWeather(widget._place);
+    }
+
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: _getInfoWidgets(_place, context),
+          children: !widget._isLoading
+              ? _getInfoWidgets(widget._place, context)
+              : [
+                  const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                ],
         ),
       ),
     );
@@ -25,44 +62,47 @@ class TodayWeatherPage extends StatelessWidget {
   List<Widget> _getInfoWidgets(Place place, BuildContext context) {
     List<Widget> list = [];
     list.add(Text(
-      _place.name,
+      widget._place.name,
       textAlign: TextAlign.center,
       style: Theme.of(context).textTheme.displayLarge,
     ));
-    if (_place.region != null) {
+    if (widget._place.region != null) {
       list.add(Text(
-        _place.region!,
+        widget._place.region!,
         textAlign: TextAlign.center,
         style: Theme.of(context).textTheme.displayLarge,
       ));
     }
-    if (_place.country != null) {
+    if (widget._place.country != null) {
       list.add(Text(
-        _place.country!,
+        widget._place.country!,
         textAlign: TextAlign.center,
         style: Theme.of(context).textTheme.displayLarge,
       ));
     }
+
     list.add(Text(
-      '30C°',
+      '${_todayWeather.currentTemperature}C°',
       textAlign: TextAlign.center,
       style: Theme.of(context).textTheme.displayLarge,
     ));
 
-    for (int i = 1; i <= 24; i++) {
+    int index = 0;
+    for (var hour in _todayWeather.hours) {
       list.add(Column(
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text('${i.toString().padLeft(2, '0')}:00'),
-              Text('0${i}C°'),
-              const Text('Sunny'),
-              Text('$i km/h'),
+              Text(hour),
+              Text('${_todayWeather.temperatures[index]}C°'),
+              Text(_todayWeather.weatherDescriptions[index]),
+              Text('${_todayWeather.windspeeds[index]} km/h'),
             ],
           ),
         ],
       ));
+      index = index + 1;
     }
 
     return list;
